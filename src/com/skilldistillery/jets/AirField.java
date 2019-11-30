939e18b4976ce04825d3c2658b8210bfa050bd11
Frank Martin <cedricmrtn@gmail.com>
2019-11-30 13:57:33 -0600

    Added txt to airfield, still working to fix printMenu(). stuck in an infinite loop.

--- src/com/skilldistillery/jets/AirField.java
+++ src/com/skilldistillery/jets/AirField.java
@@ -1,15 +1,53 @@
 package com.skilldistillery.jets;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AirField {
 	// F i e l d
-	private List<Jet> jets;
-	
-	public AirField() {
-		jets = new ArrayList<>();
+	private List<Jet> jets = new ArrayList<>();
+		String jetList = "Jets.txt";
+		
+
+	private void scanForJet() {
+		try {
+			FileReader fr = new FileReader(jetList);
+			BufferedReader br = new BufferedReader(fr);
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] jetInfo = line.split(", ");
+				String model = jetInfo[0];
+				double speed = Double.parseDouble(jetInfo[1]);
+				int range = Integer.parseInt(jetInfo[2]);
+				long price = Long.parseLong(jetInfo[3]);
+				if (line.contains("Cargo")) {
+					Jet cj = new CargoPlane(model, speed, range, price);
+					jets.add(cj);
+				}
+				if (line.contains("Fighter")) {
+					Jet fj = new FighterJet(model, speed, range, price);
+					jets.add(fj);
+				}
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			System.err.println("Invalid filename: "+ e.getMessage());
+			return;
+		}
+		catch (IOException e) {
+			System.err.println("Problem while reading "+ jetList +": " + e.getMessage());
+			return;
+		}
+		System.out.println(jetList);
+		
 	}
-	
-	
-}
+	public List<Jet> scanForJets(){
+		scanForJet();
+		return jets;
+	}
+	}
+
